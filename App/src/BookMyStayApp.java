f5aff9a61ccb37e368b0d4ec52f99ac56bc3b1aa
Pranesh-2323 <praneshhh23@gmail.com>
2026-03-16 08:52:10 +0530

    pranesh  Added welcomepage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,24 @@
+/**
+ * HotelBookingApp
+ *
+ * Entry point of the Hotel Booking Application.
+ * This program prints a welcome message with
+ * the application name and version.
+ *
+ * @author Pranesh Raj
+ * @version 1.0
+ */
+
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        String appName = "Hotel Booking System";
+        String version = "v1.0";
+
+        System.out.println("Welcome to " + appName);
+        System.out.println("Application Version: " + version);
+        System.out.println("Application started successfully.");
+        System.out.println("Application terminated.");
+    }
+}
